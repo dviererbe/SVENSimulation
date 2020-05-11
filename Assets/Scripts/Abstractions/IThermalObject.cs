f45888be97371dc36987831c6c580277d63cf7b6
Dominik Viererbe <dominik.viererbe@lin-portal.de>
2020-05-11 12:22:01 +0200

    Added Thermal Thingy

---
+++ Assets/Scripts/Abstractions/IThermalObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Abstractions
+{
+    public interface IThermalObject
+    {
+        /// <summary>
+        /// Gets if the object is static and can not change its position.
+        /// </summary>
+        bool IsStatic { get; }
+
+        /// <summary>
+        /// Gets the Position of the thermal object.
+        /// </summary>
+        Vector3 Position { get; }
+
+        /// <summary>
+        /// Gets how large the thermal object is.
+        /// </summary>
+        Vector3 Size { get; }
+
+        /// <summary>
+        /// Gets the mass of the thermal object.
+        /// </summary>
+        float Mass { get; }
+
+        /// <summary>
+        /// Gets the temperature of the thermal object.
+        /// </summary>
+        float Temperature { get; }
+
+        /// <summary>
+        /// WIP (may be removed/replaced in future versions)
+        ///
+        /// Exchanged thermal energy with the thermal object.
+        /// </summary>
+        /// <param name="energy">
+        /// Amount of thermal energy in watts. If positive the thermal object receives thermal energy and if negative it looses thermal energy.
+        /// </param>
+        void TransferHeat(float energy);
+    }
+}
---
+++ Assets/Scripts/DependencyManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Abstractions;
+
+namespace Assets.Scripts
+{
+    public class DependencyManager
+    {
+        /// <summary>
+        /// Gets the ThermalManager dependency.
+        /// </summary>
+        public IThermalManager ThermalManager { get; private set; }
+
+        /// <summary>
+        /// called when the script instance is being loaded
+        /// <see href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.Awake.html">MonoBehaviour.Awake</see>
+        /// </summary>
+        private void Awake()
+        {
+            ThermalManager = new ThermalManager();
+        }
+
+        /// <summary>
+        /// called on the frame when a script is enabled just before any of the Update methods are called the first time
+        /// <see href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.Start.html">MonoBehaviour.Start</see>
+        /// </summary>
+        private void Start()
+        {
+            ThermalManager.Start();
+        }
+
+        /// <summary>
+        /// called every frame, if the MonoBehaviour is enabled
+        /// <see href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.Update.html">MonoBehaviour.Update</see>
+        /// </summary>
+        private void Update()
+        {
+            ThermalManager.Update();
+        }
+    }
+}
