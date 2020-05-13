0f5d634d4ed17da06f5cefe14a09fcfff82854ef
Tsuksdl <54236718+Tsuksdl@users.noreply.github.com>
2020-05-13 21:53:28 +0200

    Server Connection

---
+++ Assets/Scripts/Test_Server.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Server : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ServerConnection connection = ServerConnection.getInstance();
+        Debug.Log(connection.setData("dummy_Unity", "23"));
+        Debug.Log(connection.getData("anyViews", "icon%20on"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
