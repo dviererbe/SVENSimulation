6cac52ce47df3e4cda6907b8a2b25e347353a310
Dominik Viererbe <dominik.viererbe@lin-portal.de>
2020-05-17 22:53:27 +0200

    Added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float MovementSpeed = 0.01f;
+    public float CameraZoomSpeed = 4f;
+
+    private Rigidbody2D _camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 translation = new Vector2(
+            x: Input.GetAxis("Horizontal"),
+            y: Input.GetAxis("Vertical"));
+
+        float mouseInput = Input.GetAxis("Mouse ScrollWheel");
+
+        Camera.main.fieldOfView -= mouseInput * CameraZoomSpeed;
+
+        _camera.transform.Translate(translation.normalized * MovementSpeed);
+        
+    }
+}
