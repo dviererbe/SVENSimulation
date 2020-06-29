ad02c3d607d1a3fbf0fff6f8ec5b476ce1fb9405
Tsuksdl <54236718+Tsuksdl@users.noreply.github.com>
2020-06-29 12:17:22 +0200

    Arbeit am 29.06. erstellen der Graphenklasse und der schnittberechnung

---
+++ Assets/Scripts/Pathfinder/Abstraktion/IRoomGraph.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Pathfinder
+{
+    interface IRoomGraph
+    {
+        public bool AddNode(float posY, float posX);
+
+        public bool AddSqaureObject(float posY, float posX, float rotation, float height, float width);
+
+        public void MeshList();
+
+
+    }
+}
