128fa47d36ee0ef75d6c35455334acdebbab1f11
Shortyoo <twawrik@yahoo.de>
2020-07-03 15:20:53 +0200

    Rocket Sciencegit checkout -b rocket_science

---
+++ Assets/Scripts/RocketScript.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float stepSize = -1f;
+    float angle = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, 0, angle += stepSize);
+    }
+}
