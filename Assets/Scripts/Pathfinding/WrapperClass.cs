3afb7512c745c9fcd99e5b4cbdcb0c904817d974
Shortyoo <twawrik@yahoo.de>
2020-07-03 17:23:08 +0200

    Cuz we fucked up

---
+++ Assets/Scripts/Pathfinding/WrapperClass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Assets.Scripts.Pathfinding;
+
+public class WrapperClass
+{
+    public WrapperClass(Vertex vertex, float cost)
+    {
+        this.Vertex = vertex;
+        this.Cost = cost;
+    }
+
+    public Vertex Vertex { get; private set; }
+    public float Cost { get; private set; }
+}
