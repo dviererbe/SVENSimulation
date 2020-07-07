a312be276729b58c5074fc151cc4fac8a980f965
Shortyoo <twawrik@yahoo.de>
2020-07-07 09:59:46 +0200

    Vector2 is now from UnityEngine namespace

--- Assets/Scripts/Pathfinding/Vertex.cs
+++ Assets/Scripts/Pathfinding/Vertex.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine;
 
 namespace Assets.Scripts.Pathfinding
 {
     public abstract class Vertex : IEquatable<Vertex>
     {
-        protected Vertex(Vector2 position)
+        protected Vertex(UnityEngine.Vector2 position)
         {
             Position = position;
         }
 
-        public Vector2 Position { get; }
+        public UnityEngine.Vector2 Position { get; }
 
         public abstract IReadOnlyList<Edge> Edges { get; }
 
